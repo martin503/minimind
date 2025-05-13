35a170ba74d5471a17ae7c53452d1044aa1c4a6c
martin503 <m6rcin53@gmail.com>
2025-05-13 23:26:30 +0200

    first pretrain run in 50min on both gpus

--- eval_model.py
+++ eval_model.py
@@ -1,37 +1,48 @@
 import argparse
 import random
 import warnings
-import numpy as np
-from transformers import AutoTokenizer, AutoModelForCausalLM, TextStreamer
-from model.model_minimind import MiniMindConfig, MiniMindForCausalLM
-from model.model_lora import *
 
-warnings.filterwarnings('ignore')
+import numpy as np
+from transformers import AutoModelForCausalLM, AutoTokenizer, TextStreamer
+
+from model.model_lora import *
+from model.model_minimind import MiniMindConfig, MiniMindForCausalLM
+
+# warnings.filterwarnings('ignore')
 
 
 def init_model(args):
-    tokenizer = AutoTokenizer.from_pretrained('./model/')
+    tokenizer = AutoTokenizer.from_pretrained("./model/")
     if args.load == 0:
-        moe_path = '_moe' if args.use_moe else ''
-        modes = {0: 'pretrain', 1: 'full_sft', 2: 'rlhf', 3: 'reason', 4: 'grpo'}
-        ckp = f'./{args.out_dir}/{modes[args.model_mode]}_{args.hidden_size}{moe_path}.pth'
+        moe_path = "_moe" if args.use_moe else ""
+        modes = {0: "pretrain", 1: "full_sft", 2: "rlhf", 3: "reason", 4: "grpo"}
+        ckp = (
+            args.model_path
+            or f"./{args.out_dir}/{modes[args.model_mode]}_{args.hidden_size}{moe_path}.pth"
+        )
 
-        model = MiniMindForCausalLM(MiniMindConfig(
-            hidden_size=args.hidden_size,
-            num_hidden_layers=args.num_hidden_layers,
-            use_moe=args.use_moe
-        ))
+        model = MiniMindForCausalLM(
+            MiniMindConfig(
+                hidden_size=args.hidden_size,
+                num_hidden_layers=args.num_hidden_layers,
+                use_moe=args.use_moe,
+            )
+        )
 
         model.load_state_dict(torch.load(ckp, map_location=args.device), strict=True)
 
-        if args.lora_name != 'None':
+        if args.lora_name != "None":
             apply_lora(model)
-            load_lora(model, f'./{args.out_dir}/lora/{args.lora_name}_{args.hidden_size}.pth')
+            load_lora(model, f"./{args.out_dir}/lora/{args.lora_name}_{args.hidden_size}.pth")
     else:
-        transformers_model_path = './MiniMind2'
+        transformers_model_path = args.model_path or "./MiniMind2"
         tokenizer = AutoTokenizer.from_pretrained(transformers_model_path)
-        model = AutoModelForCausalLM.from_pretrained(transformers_model_path, trust_remote_code=True)
-    print(f'MiniMind model parameter quantity : {sum(p.numel() for p in model.parameters() if p.requires_grad) / 1e6:.2f}M(illion)')
+        model = AutoModelForCausalLM.from_pretrained(
+            transformers_model_path, trust_remote_code=True
+        )
+    print(
+        f"MiniMind model parameter quantity : {sum(p.numel() for p in model.parameters() if p.requires_grad) / 1e6:.2f}M(illion)"
+    )
     return model.eval().to(args.device), tokenizer
 
 
@@ -39,45 +50,45 @@ def get_prompt_datas(args):
     if args.model_mode == 0:
         # pretrain model's solitaire ability （ unable to talk ）
         prompt_datas = [
-            ' the basic principles of marxism ',
-            ' the main functions of the human brain ',
-            ' the principle of gravity is ',
-            ' the highest mountain in the world is ',
-            ' carbon dioxide in the air ',
-            ' the largest animal on earth is ',
-            ' the food in hangzhou is '
+            " the basic principles of marxism ",
+            " the main functions of the human brain ",
+            " the principle of gravity is ",
+            " the highest mountain in the world is ",
+            " carbon dioxide in the air ",
+            " the largest animal on earth is ",
+            " the food in hangzhou is ",
         ]
     else:
-        if args.lora_name == 'None':
+        if args.lora_name == "None":
             #  general conversation issues
             prompt_datas = [
-                ' please introduce yourself 。',
-                ' which subject are you better at ？',
+                " please introduce yourself 。",
+                " which subject are you better at ？",
                 " lu xun's 《 madman's diary 》 how to criticize feudal ethics ？",
-                ' my cough has been going on for two weeks ， do you need to go to the hospital for examination? ？',
-                ' a detailed introduction to the physical concept of the speed of light 。',
-                ' recommend some special foods in hangzhou 。',
-                ' Please explain the “Large Language Model” to me this concept 。',
-                ' how to understand ChatGPT？',
-                'Introduce the history of the United States, please.'
+                " my cough has been going on for two weeks ， do you need to go to the hospital for examination? ？",
+                " a detailed introduction to the physical concept of the speed of light 。",
+                " recommend some special foods in hangzhou 。",
+                " Please explain the “Large Language Model” to me this concept 。",
+                " how to understand ChatGPT？",
+                "Introduce the history of the United States, please.",
             ]
         else:
             #  area-specific issues
             lora_prompt_datas = {
-                'lora_identity': [
+                "lora_identity": [
                     " who are you ChatGPT bar 。",
                     " may i have your name ？",
-                    " you and openai what's the relationship ？"
+                    " you and openai what's the relationship ？",
                 ],
-                'lora_medical': [
+                "lora_medical": [
                     " i've been feeling dizzy lately ， what might be the reason ？",
-                    ' my cough has been going on for two weeks ， do you need to go to the hospital for examination? ？',
-                    ' what should i pay attention to when taking antibiotics ？',
-                    ' the physical examination report shows that the cholesterol is high ， what do i do ？',
-                    ' what should pregnant women pay attention to in their diet ？',
-                    ' how to prevent osteoporosis in the elderly ？',
+                    " my cough has been going on for two weeks ， do you need to go to the hospital for examination? ？",
+                    " what should i pay attention to when taking antibiotics ？",
+                    " the physical examination report shows that the cholesterol is high ， what do i do ？",
+                    " what should pregnant women pay attention to in their diet ？",
+                    " how to prevent osteoporosis in the elderly ？",
                     " i've always felt anxious lately ， how to alleviate ？",
-                    ' if someone faints suddenly ， how to first rescue ？'
+                    " if someone faints suddenly ， how to first rescue ？",
                 ],
             }
             prompt_datas = lora_prompt_datas[args.lora_name]
@@ -98,56 +109,62 @@ def setup_seed(seed):
 
 def main():
     parser = argparse.ArgumentParser(description="Chat with MiniMind")
-    parser.add_argument('--lora_name', default='None', type=str)
-    parser.add_argument('--out_dir', default='out', type=str)
-    parser.add_argument('--temperature', default=0.85, type=float)
-    parser.add_argument('--top_p', default=0.85, type=float)
-    parser.add_argument('--device', default='cuda' if torch.cuda.is_available() else 'cpu', type=str)
+    parser.add_argument("--lora_name", default="None", type=str)
+    parser.add_argument("--out_dir", default="out", type=str)
+    parser.add_argument("--temperature", default=0.85, type=float)
+    parser.add_argument("--top_p", default=0.85, type=float)
+    parser.add_argument(
+        "--device", default="cuda" if torch.cuda.is_available() else "cpu", type=str
+    )
     #  here max_seq_len（ maximum output length ） does not mean that the model has the corresponding long text performance ， prevent only QA there is a problem of being truncated
     # MiniMind2-moe (145M)：(hidden_size=640, num_hidden_layers=8, use_moe=True)
     # MiniMind2-Small (26M)：(hidden_size=512, num_hidden_layers=8)
     # MiniMind2 (104M)：(hidden_size=768, num_hidden_layers=16)
-    parser.add_argument('--hidden_size', default=512, type=int)
-    parser.add_argument('--num_hidden_layers', default=8, type=int)
-    parser.add_argument('--max_seq_len', default=8192, type=int)
-    parser.add_argument('--use_moe', default=False, type=bool)
+    parser.add_argument("--hidden_size", default=512, type=int)
+    parser.add_argument("--num_hidden_layers", default=8, type=int)
+    parser.add_argument("--max_seq_len", default=8192, type=int)
+    parser.add_argument("--use_moe", default=False, type=bool)
     #  carry historical dialogue context number
     # history_cnt need to be set to even number ， right now 【 user problems ,  model answer 】 for 1 group ； set as 0 hour ， that is the current query don't carry historical texts
     #  when the model has not been extrapolated and fine-tuned ， in longer context chat_template it is inevitable that the performance will be significantly degraded ， therefore, you need to pay attention to the settings here
-    parser.add_argument('--history_cnt', default=0, type=int)
-    parser.add_argument('--load', default=0, type=int, help="0:  native torch weight ，1: transformers load ")
-    parser.add_argument('--model_mode', default=1, type=int,
-                        help="0:  pre-trained model ，1: SFT-Chat model ，2: RLHF-Chat model ，3: Reason model ，4: RLAIF-Chat model ")
+    parser.add_argument("--history_cnt", default=0, type=int)
+    parser.add_argument(
+        "--load", default=0, type=int, help="0:  native torch weight ，1: transformers load "
+    )
+    parser.add_argument(
+        "--model_mode",
+        default=1,
+        type=int,
+        help="0:  pre-trained model ，1: SFT-Chat model ，2: RLHF-Chat model ，3: Reason model ，4: RLAIF-Chat model ",
+    )
+    parser.add_argument("--model_path", type=str, help="Path to model which should be loaded")
     args = parser.parse_args()
 
     model, tokenizer = init_model(args)
 
     prompts = get_prompt_datas(args)
-    test_mode = int(input('[0]  automatic testing \n[1]  manual input \n'))
+    test_mode = int(input("[0]  automatic testing \n[1]  manual input \n"))
     streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
 
     messages = []
-    for idx, prompt in enumerate(prompts if test_mode == 0 else iter(lambda: input('👶: '), '')):
+    for idx, prompt in enumerate(prompts if test_mode == 0 else iter(lambda: input("👶: "), "")):
         setup_seed(random.randint(0, 2048))
         # setup_seed(2025)  #  if you need to fix each output, change it to 【 fixed 】 random seeds of
-        if test_mode == 0: print(f'👶: {prompt}')
+        if test_mode == 0:
+            print(f"👶: {prompt}")
 
-        messages = messages[-args.history_cnt:] if args.history_cnt else []
+        messages = messages[-args.history_cnt :] if args.history_cnt else []
         messages.append({"role": "user", "content": prompt})
 
-        new_prompt = tokenizer.apply_chat_template(
-            messages,
-            tokenize=False,
-            add_generation_prompt=True
-        ) if args.model_mode != 0 else (tokenizer.bos_token + prompt)
+        new_prompt = (
+            tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+            if args.model_mode != 0
+            else (tokenizer.bos_token + prompt)
+        )
 
-        inputs = tokenizer(
-            new_prompt,
-            return_tensors="pt",
-            truncation=True
-        ).to(args.device)
+        inputs = tokenizer(new_prompt, return_tensors="pt", truncation=True).to(args.device)
 
-        print('🤖️: ', end='')
+        print("🤖️: ", end="")
         generated_ids = model.generate(
             inputs["input_ids"],
             max_new_tokens=args.max_seq_len,
@@ -158,12 +175,14 @@ def main():
             eos_token_id=tokenizer.eos_token_id,
             streamer=streamer,
             top_p=args.top_p,
-            temperature=args.temperature
+            temperature=args.temperature,
         )
 
-        response = tokenizer.decode(generated_ids[0][inputs["input_ids"].shape[1]:], skip_special_tokens=True)
+        response = tokenizer.decode(
+            generated_ids[0][inputs["input_ids"].shape[1] :], skip_special_tokens=True
+        )
         messages.append({"role": "assistant", "content": response})
-        print('\n\n')
+        print("\n\n")
 
 
 if __name__ == "__main__":
--- trainer/train_pretrain.py
+++ trainer/train_pretrain.py
@@ -1,23 +1,22 @@
+import argparse
+import math
 import os
 import sys
-__package__ = "trainer"
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-import argparse
 import time
-import math
 import warnings
+from contextlib import nullcontext
+
 import torch
 import torch.distributed as dist
-from torch import optim, nn
+from torch import nn, optim
 from torch.nn.parallel import DistributedDataParallel
 from torch.utils.data import DataLoader, DistributedSampler
-from contextlib import nullcontext
 from transformers import AutoTokenizer
-from model.model_minimind import MiniMindConfig, MiniMindForCausalLM
-from dataset.lm_dataset import PretrainDataset
 
-warnings.filterwarnings('ignore')
+from dataset.lm_dataset import PretrainDataset
+from model.model_minimind import MiniMindConfig, MiniMindForCausalLM
+
+# warnings.filterwarnings('ignore')
 
 
 def Logger(content):
@@ -30,23 +29,22 @@ def get_lr(current_step, total_steps, lr):
 
 
 def train_epoch(epoch, wandb):
-    loss_fct = nn.CrossEntropyLoss(reduction='none')
+    loss_fct = nn.CrossEntropyLoss(reduction="none")
     start_time = time.time()
     for step, (X, Y, loss_mask) in enumerate(train_loader):
         X = X.to(args.device)
         Y = Y.to(args.device)
         loss_mask = loss_mask.to(args.device)
 
-        lr = get_lr(epoch * iter_per_epoch + step, args.epochs * iter_per_epoch, args.learning_rate)
+        lr = get_lr(
+            epoch * iter_per_epoch + step, args.epochs * iter_per_epoch, args.learning_rate
+        )
         for param_group in optimizer.param_groups:
-            param_group['lr'] = lr
+            param_group["lr"] = lr
 
         with ctx:
             res = model(X)
-            loss = loss_fct(
-                res.logits.view(-1, res.logits.size(-1)),
-                Y.view(-1)
-            ).view(Y.size())
+            loss = loss_fct(res.logits.view(-1, res.logits.size(-1)), Y.view(-1)).view(Y.size())
             loss = (loss * loss_mask).sum() / loss_mask.sum()
             loss += res.aux_loss
             loss = loss / args.accumulation_steps
@@ -65,44 +63,54 @@ def train_epoch(epoch, wandb):
         if step % args.log_interval == 0:
             spend_time = time.time() - start_time
             Logger(
-                'Epoch:[{}/{}]({}/{}) loss:{:.3f} lr:{:.12f} epoch_Time:{}min:'.format(
+                "Epoch:[{}/{}]({}/{}) loss:{:.3f} lr:{:.12f} epoch_Time:{}min:".format(
                     epoch + 1,
                     args.epochs,
                     step,
                     iter_per_epoch,
                     loss.item() * args.accumulation_steps,
-                    optimizer.param_groups[-1]['lr'],
-                    spend_time / (step + 1) * iter_per_epoch // 60 - spend_time // 60))
+                    optimizer.param_groups[-1]["lr"],
+                    spend_time / (step + 1) * iter_per_epoch // 60 - spend_time // 60,
+                )
+            )
 
             if (wandb is not None) and (not ddp or dist.get_rank() == 0):
-                wandb.log({"loss": loss.item() * args.accumulation_steps,
-                           "lr": optimizer.param_groups[-1]['lr'],
-                           "epoch_Time": spend_time / (step + 1) * iter_per_epoch // 60 - spend_time // 60})
+                wandb.log(
+                    {
+                        "loss": loss.item() * args.accumulation_steps,
+                        "lr": optimizer.param_groups[-1]["lr"],
+                        "epoch_Time": spend_time / (step + 1) * iter_per_epoch // 60
+                        - spend_time // 60,
+                    }
+                )
 
         if (step + 1) % args.save_interval == 0 and (not ddp or dist.get_rank() == 0):
             model.eval()
-            moe_path = '_moe' if lm_config.use_moe else ''
-            ckp = f'{args.save_dir}/pretrain_{lm_config.hidden_size}{moe_path}.pth'
+            moe_path = "_moe" if lm_config.use_moe else ""
+            ckp = f"{args.save_dir}/pretrain_{lm_config.hidden_size}{moe_path}.pth"
 
             if isinstance(model, torch.nn.parallel.DistributedDataParallel):
                 state_dict = model.module.state_dict()
             else:
                 state_dict = model.state_dict()
 
-            state_dict = {k: v.half() for k, v in state_dict.items()}  #  half precision saving 
+            state_dict = {k: v.half() for k, v in state_dict.items()}  #  half precision saving
             torch.save(state_dict, ckp)
             model.train()
 
 
 def init_model(lm_config):
-    tokenizer = AutoTokenizer.from_pretrained('../model/')
+    tokenizer = AutoTokenizer.from_pretrained("model/")
     model = MiniMindForCausalLM(lm_config).to(args.device)
-    Logger(f'LLM total training parameters ：{sum(p.numel() for p in model.parameters() if p.requires_grad) / 1e6:.3f}  million ')
+    Logger(
+        f"LLM total training parameters ：{sum(p.numel() for p in model.parameters() if p.requires_grad) / 1e6:.3f}  million "
+    )
     return model, tokenizer
 
 
 def init_distributed_mode():
-    if not ddp: return
+    if not ddp:
+        return
     global ddp_local_rank, DEVICE
 
     dist.init_process_group(backend="nccl")
@@ -117,30 +125,37 @@ def init_distributed_mode():
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="MiniMind Pretraining")
     parser.add_argument("--out_dir", type=str, default="../out")
-    #  to achieve it at the fastest speed zero but epochs set as 1 wheel ； otherwise, limited data should be used to train 2~6 indivual epochs。
+    #  to achieve it at the fastest speed zero but epochs set as 1 wheel ； otherwise, limited data should be used to train 2~6 epochs
     parser.add_argument("--epochs", type=int, default=1)
     parser.add_argument("--batch_size", type=int, default=32)
     parser.add_argument("--learning_rate", type=float, default=5e-4)
-    parser.add_argument("--device", type=str, default="cuda:0" if torch.cuda.is_available() else "cpu")
+    parser.add_argument(
+        "--device", type=str, default="cuda:0" if torch.cuda.is_available() else "cpu"
+    )
     parser.add_argument("--dtype", type=str, default="bfloat16")
     parser.add_argument("--use_wandb", action="store_true")
-    parser.add_argument("--wandb_project", type=str, default="MiniMind-Pretrain")
+    parser.add_argument("--wandb_project", type=str, default="minimind")
     parser.add_argument("--num_workers", type=int, default=1)
-    parser.add_argument("--ddp", action="store_true")
     parser.add_argument("--accumulation_steps", type=int, default=8)
     parser.add_argument("--grad_clip", type=float, default=1.0)
     parser.add_argument("--warmup_iters", type=int, default=0)
     parser.add_argument("--log_interval", type=int, default=100)
     parser.add_argument("--save_interval", type=int, default=100)
-    parser.add_argument('--local_rank', type=int, default=-1)
-    parser.add_argument('--hidden_size', default=512, type=int)
-    parser.add_argument('--num_hidden_layers', default=8, type=int)
-    parser.add_argument('--max_seq_len', default=512, type=int)
-    parser.add_argument('--use_moe', default=False, type=bool)
-    parser.add_argument("--data_path", type=str, default="../dataset/pretrain_hq.jsonl")
+    parser.add_argument("--local_rank", type=int, default=-1)
+    parser.add_argument("--hidden_size", default=512, type=int)
+    parser.add_argument("--num_hidden_layers", default=8, type=int)
+    parser.add_argument("--max_seq_len", default=512, type=int)
+    parser.add_argument("--use_moe", default=False, type=bool)
+    parser.add_argument(
+        "--data_path", type=str, default="/home/komikadze/datasets/minimind/pretrain_hq.jsonl"
+    )
     args = parser.parse_args()
 
-    lm_config = MiniMindConfig(hidden_size=args.hidden_size, num_hidden_layers=args.num_hidden_layers, use_moe=args.use_moe)
+    lm_config = MiniMindConfig(
+        hidden_size=args.hidden_size,
+        num_hidden_layers=args.num_hidden_layers,
+        use_moe=args.use_moe,
+    )
     args.save_dir = os.path.join(args.out_dir)
     os.makedirs(args.save_dir, exist_ok=True)
     os.makedirs(args.out_dir, exist_ok=True)
@@ -163,7 +178,7 @@ if __name__ == "__main__":
         args.device = torch.device(DEVICE)
         rank = dist.get_rank()
         torch.manual_seed(base_seed + rank)
-        #  setting it simultaneously  CUDA  random seeds of 
+        #  setting it simultaneously  CUDA  random seeds of
         torch.cuda.manual_seed(base_seed + rank)
 
     if args.use_wandb and (not ddp or ddp_local_rank == 0):
@@ -183,10 +198,10 @@ if __name__ == "__main__":
         drop_last=False,
         shuffle=False,
         num_workers=args.num_workers,
-        sampler=train_sampler
+        sampler=train_sampler,
     )
 
-    scaler = torch.cuda.amp.GradScaler(enabled=(args.dtype in ['float16', 'bfloat16']))
+    scaler = torch.cuda.amp.GradScaler(enabled=(args.dtype in ["float16", "bfloat16"]))
     optimizer = optim.AdamW(model.parameters(), lr=args.learning_rate)
 
     if ddp:
